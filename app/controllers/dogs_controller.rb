2bf203ba62f73eb95fe982d4b4b0b1b50bf2c3d3
Edgar <edgar.a.rivera08@gmail.com>
2020-04-29 11:25:58 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        # raise params.inspect
+        @dog = Dog.find(params[:id])
+    end
 end
