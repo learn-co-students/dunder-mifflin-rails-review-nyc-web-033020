d458141c9a996bcbd903fc68985f952996ee41e3
dkersten <dkersten1994@gmail.com>
2020-04-29 20:31:28 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.assign_attributes(employee_params)
+
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to edit_employee_path
+        end
+
+    end
+
+    private
+
+        def employee_params
+            params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+        end
+
 end
